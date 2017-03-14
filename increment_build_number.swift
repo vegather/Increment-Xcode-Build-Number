4538dfb9a378b24a7f4d4795500cd310fa9cbaa2
Vegard Solheim Theriault <vegather@icloud.com>
2017-03-14 10:55:47 +0100

    Fixed typo

--- increment_build_number.swift
+++ increment_build_number.swift
@@ -1,14 +1,13 @@
 import Foundation
 
 func incrementBuildNumber() {
-    
-    let currentPath = FileManager.default.currentDirectoryPath
-    guard let projectName = currentPath.components(separatedBy: "/").last else { return }
+	let currentPath = FileManager.default.currentDirectoryPath
+	guard let projectName = currentPath.components(separatedBy: "/").last else { return }
 
 	let infoPlistPath = currentPath + "/" + projectName + "/info.plist"
 	guard var infoPlist = NSDictionary(contentsOfFile: infoPlistPath) as? [String: Any] else { return }
-	
-    let buildNumber = Int(infoPlist["CFBundleVersion"] as? String ?? "") ?? 0
+
+	let buildNumber = Int(infoPlist["CFBundleVersion"] as? String ?? "") ?? 0
 	infoPlist["CFBundleVersion"] = String(buildNumber + 1)
     
 	(infoPlist as NSDictionary).write(toFile: infoPlistPath, atomically: true)
